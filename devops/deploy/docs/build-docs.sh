1f5c090e1e1eba423e1b1fe8b88687cc8db0b6e1
tuke <bobby.j.strange@gmail.com>
2021-06-18 00:52:31 +0800

    stash

--- devops/deploy/docs/build-docs.sh
+++ devops/deploy/docs/build-docs.sh
@@ -13,6 +13,9 @@ fi
 cd `dirname $0`
 WORK_DIR=`pwd`
 
+# copy auth to current directory
+cp -r ../../../../../pepper/rita-auth .
+
 # cd to PROJECT_DIR
 cd ../../..
 rm -rf apidoc/
@@ -20,9 +23,11 @@ apidoc -o apidoc/ --parse-languages java
 
 cd $WORK_DIR
 cp -r ../../../apidoc .
+rm -rf rita-auth
 
 VERSION="0.1.0"
-REGISTRY_PREFIX="registry.cn-hangzhou.aliyuncs.com/tuke"
+#REGISTRY_PREFIX="registry.cn-hangzhou.aliyuncs.com/tuke"
+REGISTRY_PREFIX="tukeof"
 IMAGE_NAME="$REGISTRY_PREFIX/maid-docs:$VERSION"
 $BIN $IMAGE_NAME .
 docker push $IMAGE_NAME
